3123dee32a5600d6ea3e93b2537ecc8263ef666c
stillonwarpath <fr.betancourt@live.com>
2023-03-30 23:38:36 -0300

    Added container for dependency injection. Used container to get status get controller instance.

--- src/apps/mooc/backend/routes/status.route.ts
+++ src/apps/mooc/backend/routes/status.route.ts
@@ -1,8 +1,8 @@
 import { Request, Response, Router } from 'express';
-
 import { StatusGetController } from '../controllers/StatusGetController';
+import container from '../dependency-injection';
 
 export const register = (router: Router) => {
-	const controller = new StatusGetController();
+	const controller: StatusGetController = container.get('Apps.mooc.controllers.StatusGetController');
 	router.get('/status', (req: Request, res: Response) => controller.run(req, res));
 };
