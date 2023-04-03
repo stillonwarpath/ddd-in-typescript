687b7e0c74bc9f0ea669d18a8b8520b5e5ab85d9
stillonwarpath <fr.betancourt@live.com>
2023-04-03 16:34:30 -0400

    Called course creator use case from courses put controller class. Added dependency injection of course creator.

--- src/apps/mooc/backend/controllers/CoursesPutController.ts
+++ src/apps/mooc/backend/controllers/CoursesPutController.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from 'express';
 
 import { Controller } from './Controller';
+import { CourseCreator } from '../../../../Contexts/Mooc/Courses/application/CourseCreator';
+import httpStatus from 'http-status';
 
 export class CoursesPutController implements Controller {
+
+	constructor(private courseCreator: CourseCreator) {}
+
 	async run(req: Request, res: Response): Promise<void> {
-		throw new Error('method not implemented.');
+		const { id, name, duration } = req.body;
+		
+		await this.courseCreator.run(id, name, duration);
+
+		res.status(httpStatus.CREATED).send();
 	}
 }
