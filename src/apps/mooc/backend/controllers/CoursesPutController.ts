1483cbd4a5ed951db1f842e0f8108f71b63b5679
stillonwarpath <fr.betancourt@live.com>
2023-04-02 20:43:40 -0400

    Implemented course creator use case.

--- src/apps/mooc/backend/controllers/CoursesPutController.ts
+++ src/apps/mooc/backend/controllers/CoursesPutController.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from 'express';
 
 import { Controller } from './Controller';
-import httpStatus from 'http-status';
 
 export class CoursesPutController implements Controller {
 	async run(req: Request, res: Response): Promise<void> {
-		res.send(httpStatus.CREATED).send();
+		throw new Error('method not implemented.');
 	}
 }
--- tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
+++ tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
@@ -1,25 +1,20 @@
 import { CourseCreator } from '../../../../../src/Contexts/Mooc/Courses/application/CourseCreator';
 import { Course } from '../../../../../src/Contexts/Mooc/Courses/domain/Course';
-import { CourseRepositoryMock } from '../__mocks__/CourseRepositoryMock';
-
-let repository: CourseRepositoryMock;
-let creator: CourseCreator;
-
-beforeEach(() => {
-	repository = new CourseRepositoryMock();
-	creator = new CourseCreator(repository);
-});
+import { CourseRepository } from '../../../../../src/Contexts/Mooc/Courses/domain/CourseRepository';
 
 describe('CourseCreator', () => {
 	it('should create a valid course', async () => {
-		const id = 'some-id';
-		const name = 'some-name';
-		const duration = 'some-duration';
-
-		const course = new Course({ id, name, duration });
+		const repository: CourseRepository = {
+			save: jest.fn()
+		};
+		const creator = new CourseCreator(repository);
+		const id = 'id';
+		const name = 'name';
+		const duration = '5 hours';
+		const expectedCourse = new Course(id, name, duration);
 
 		await creator.run(id, name, duration);
 
-		repository.assertLastSavedCourseIs(course);
+		expect(repository.save).toHaveBeenCalledWith(expectedCourse);
 	});
 });
