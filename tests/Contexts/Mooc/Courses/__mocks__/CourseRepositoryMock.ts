f5cf0b28f36efbfc1c329b664a1deeae8585d606
stillonwarpath <fr.betancourt@live.com>
2023-04-05 22:24:15 -0400

    Refactor of test adding mock of course repository.

---
+++ tests/Contexts/Mooc/Courses/__mocks__/CourseRepositoryMock.ts
@@ -0,0 +1,18 @@
+import { Course } from '../../../../../src/Contexts/Mooc/Courses/domain/Course';
+import { CourseRepository } from '../../../../../src/Contexts/Mooc/Courses/domain/CourseRepository';
+
+export class CourseRepositoryMock implements CourseRepository {
+	private readonly saveMock: jest.Mock;
+
+	constructor() {
+		this.saveMock = jest.fn();
+	}
+
+	async save(course: Course): Promise<void> {
+		this.saveMock(course);
+	}
+
+	assertSaveHaveBeenCalledWith(expected: Course): void {
+		expect(this.saveMock).toHaveBeenCalledWith(expected);
+	}
+}
--- tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
+++ tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
@@ -1,20 +1,24 @@
 import { CourseCreator } from '../../../../../src/Contexts/Mooc/Courses/application/CourseCreator';
 import { Course } from '../../../../../src/Contexts/Mooc/Courses/domain/Course';
-import { CourseRepository } from '../../../../../src/Contexts/Mooc/Courses/domain/CourseRepository';
+import { CourseRepositoryMock } from '../__mocks__/CourseRepositoryMock';
 
 describe('CourseCreator', () => {
+
+	let repository: CourseRepositoryMock;
+
+	beforeEach(() => {
+		repository = new CourseRepositoryMock();
+	});
+
 	it('should create a valid course', async () => {
-		const repository: CourseRepository = {
-			save: jest.fn()
-		};
 		const creator = new CourseCreator(repository);
 		const id = 'id';
 		const name = 'name';
 		const duration = '5 hours';
-		const expectedCourse = new Course(id, name, duration);
+		const expectedCourse = new Course("fakeId", name, duration);
 
 		await creator.run(id, name, duration);
 
-		expect(repository.save).toHaveBeenCalledWith(expectedCourse);
+		repository.assertSaveHaveBeenCalledWith(expectedCourse);
 	});
 });
