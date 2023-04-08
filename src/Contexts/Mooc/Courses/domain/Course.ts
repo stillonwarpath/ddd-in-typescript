af61f04719253784b0402196fe3eb29030062a2e
stillonwarpath <fr.betancourt@live.com>
2023-04-08 17:38:47 -0400

    Refactor of arguments of course aggregator constructor as parameter object.

--- src/Contexts/Mooc/Courses/domain/Course.ts
+++ src/Contexts/Mooc/Courses/domain/Course.ts
@@ -1,11 +1,11 @@
-import { Uuid } from "../../../Shared/domain/value-objects/Uuid";
+import { Uuid } from '../../../Shared/domain/value-objects/Uuid';
 
 export class Course {
 	readonly id: Uuid;
 	readonly name: string;
 	readonly duration: string;
 
-	constructor(id: Uuid, name: string, duration: string) {
+	constructor({ id, name, duration }: { id: Uuid; name: string; duration: string }) {
 		this.id = id;
 		this.name = name;
 		this.duration = duration;
