c0c23b15f7581020bf1d5d917ce1fd8e273b4ec0
stillonwarpath <fr.betancourt@live.com>
2023-04-16 18:18:10 -0400

    Refactor of course repository mock.

--- tests/Contexts/Mooc/Courses/__mocks__/CourseRepositoryMock.ts
+++ tests/Contexts/Mooc/Courses/__mocks__/CourseRepositoryMock.ts
@@ -2,17 +2,17 @@ import { Course } from '../../../../../src/Contexts/Mooc/Courses/domain/Course';
 import { CourseRepository } from '../../../../../src/Contexts/Mooc/Courses/domain/CourseRepository';
 
 export class CourseRepositoryMock implements CourseRepository {
-	private readonly saveMock: jest.Mock;
-
-	constructor() {
-		this.saveMock = jest.fn();
-	}
+	
+	private mockSave = jest.fn();
 
 	async save(course: Course): Promise<void> {
-		this.saveMock(course);
+		this.mockSave(course);
 	}
 
-	assertSaveHaveBeenCalledWith(expected: Course): void {
-		expect(this.saveMock).toHaveBeenCalledWith(expected);
+	assertLastSavedCourseIs(expected: Course): void {
+		const mock = this.mockSave.mock;
+		const lastSavedCourse = mock.calls[mock.calls.length - 1][0] as Course;
+		expect(lastSavedCourse).toBeInstanceOf(Course);
+		expect(lastSavedCourse.id).toEqual(expected.id);
 	}
 }
